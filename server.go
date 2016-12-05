f9b39c327ab5f73354a7fa34396fb91ea4e02faa
Udara Bibile <chathuranga94@live.com>
2016-12-05 17:33:05 +0530

    Sample server running

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, HTTP Server Running... I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+    fmt.Println("Server running localhost:8080 ")
+    
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
